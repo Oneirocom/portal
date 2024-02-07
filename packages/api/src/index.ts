f608a135fdfd378abc9deb2a00524a71b516c5f0
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-07 14:40:30 -0500

    export some utils from portal-api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -14,3 +14,5 @@ export type RouterInputs = inferRouterInputs<AppRouter>
  * @example type HelloOutput = RouterOutputs['example']['hello']
  **/
 export type RouterOutputs = inferRouterOutputs<AppRouter>
+
+export * from './lib/utils/shared'
