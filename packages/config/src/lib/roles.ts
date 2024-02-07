ff62710ab7b201f83278aa99345ead700bad8bf2
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-06 20:57:56 -0500

    portal-config

---
+++ packages/config/src/lib/roles.ts
@@ -0,0 +1,6 @@
+export type Role = 'ANONYMOUS' | 'USER' | 'TESTER' | 'ADMIN'
+
+export const ADMIN = 'ADMIN'
+export const USER = 'USER'
+export const TESTER = 'TESTER'
+export const ANONYMOUS = 'ANONYMOUS'
