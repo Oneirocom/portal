a6d582f11002cbe7c8b55a6ae1497bc3a0857e08
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-07 01:17:57 -0500

    setup next.config

--- apps/portal/next.config.js
+++ apps/portal/next.config.js
@@ -2,21 +2,43 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next')
+const withTM = require('next-transpile-modules')(['pdfjs-dist'])
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  images: {
+    domains: [
+      'replicate.delivery',
+      'emawtpjmqvwygksvurku.supabase.co',
+      'lglrzlrsfxchbqsslvur.supabase.co',
+      'i.ytimg.com',
+      'pub-58d22deb43dc48e792b7b7468610b5f9.r2.dev',
+      'directus-production-1d52.up.railway.app',
+    ],
+  },
+  reactStrictMode: false,
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    config.module.rules.push({
+      test: [/pdfjs-dist\/build\/pdf\.worker\.js$/],
+      type: 'asset/resource',
+    })
+    config.experiments = {
+      layers: true,
+    }
+
+    return config
   },
 }
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-]
-
-module.exports = composePlugins(...plugins)(nextConfig)
+module.exports = composePlugins(withNx, withTM)(nextConfig)
