67cf2b4281d5e659d16f888f697cb010c682b8fd
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-07 17:19:33 -0500

    some projects page updates

--- apps/portal/src/pages/projects/index.tsx
+++ apps/portal/src/pages/projects/index.tsx
@@ -2,6 +2,7 @@ import { authOptions } from '@magickml/portal-auth'
 import { GetServerSidePropsContext } from 'next'
 import { getServerSession } from 'next-auth'
 import { ProjectsPage } from '@magickml/portal-pages'
+import { unauthorizedRedirect } from '@magickml/portal-utils-server'
 
 export default ProjectsPage
 
@@ -9,22 +10,20 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const session = await getServerSession(ctx.req, ctx.res, authOptions)
 
   if (!session) {
-    return {
-      redirect: {
-        destination: '/auth/sign-in',
-        permanent: false,
-      },
-    }
+    return unauthorizedRedirect({
+      destination: '/auth/sign-in',
+      message: 'notLoggedIn',
+    })
   }
 
-  if (session?.user.role !== 'ADMIN') {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
+  // if (session?.user.role !== 'ADMIN') {
+  //   return {
+  //     redirect: {
+  //       destination: '/',
+  //       permanent: false,
+  //     },
+  //   }
+  // }
 
   return {
     props: {},
