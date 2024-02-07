4647c40a62413195d2b9c225ac551da131692037
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-06 20:56:39 -0500

    portal-state

---
+++ packages/state/src/lib/agentChatState.ts
@@ -0,0 +1,29 @@
+import { atom } from 'jotai'
+import { atomWithReset } from 'jotai/utils'
+import { type Agent, type AgentMessages } from '@magickml/portal-types'
+
+// Creating an atom for the agent data
+export const agentDataAtom = atomWithReset<Agent>({
+  id: '',
+  workspace_id: '',
+  rootSpell: null,
+  publicVariables: null,
+  secrets: null,
+  name: null,
+  image: null,
+  enabled: null,
+  updatedAt: null,
+  pingedAt: null,
+  projectId: null,
+  spells: null,
+  data: null,
+  creatorName: null,
+  description: null,
+})
+
+export const agentMessagesAtom = atomWithReset<AgentMessages[]>([])
+
+export const toggleAgentChatAtom = atom(true)
+export const toggleAgentSettingsAtom = atom(true)
+export const toggleAgentMobileSettingsAtom = atom(false)
+export const settingsSheetAtom = atom(0)
