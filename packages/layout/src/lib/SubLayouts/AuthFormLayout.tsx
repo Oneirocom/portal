bbeb7ca7a82b1ea422de717a674a62ca435aec8f
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-07 02:47:30 -0500

    fix forgot-password

---
+++ packages/layout/src/lib/SubLayouts/AuthFormLayout.tsx
@@ -0,0 +1,115 @@
+import { AuthLayout } from './AuthLayout'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Fragment, useEffect } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+  formInputStyles,
+  Input,
+  Label,
+  Button,
+} from '@magickml/portal-ui'
+import { useForm } from 'react-hook-form'
+import { useSession } from 'next-auth/react'
+import { IoIosArrowBack } from 'react-icons/io'
+
+const emailFormSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+})
+
+type AuthFormLayoutProps = {
+  onSubmit: (values: FormValues) => void
+  title: string
+  description?: string
+  submitText?: string
+  isLoading?: boolean
+}
+
+type FormValues = z.infer<typeof emailFormSchema>
+
+export const AuthFormLayout = ({
+  onSubmit,
+  title,
+  description,
+  submitText,
+  isLoading = false,
+}: AuthFormLayoutProps) => {
+  const form = useForm<z.infer<typeof emailFormSchema>>({
+    resolver: zodResolver(emailFormSchema),
+    defaultValues: { email: '' },
+  })
+  const session = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session.data) router.push('/')
+  }, [])
+
+  return (
+    <AuthLayout title="">
+      <Fragment>
+        <div className="flex flex-col self-stretch w-full gap-3">
+          <div className="inline-flex items-center justify-start gap-2 dark:text-ds-primary-m text-ds-primary-p pb-6 text-sm font-medium">
+            <IoIosArrowBack className="mt-1" />
+            <Link
+              className="focus:underline hover:underline"
+              href="/auth/sign-in"
+            >
+              Back to Sign In
+            </Link>
+          </div>
+
+          <Label className="mb-2 text-3xl font-bold text-center text-black md:text-left dark:text-white font-montserrat">
+            {title}
+          </Label>
+          <p className="text-sm text-center font-medium font-montserrat pb-1">
+            {description ?? ''}
+          </p>
+
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-8"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        className={formInputStyles}
+                        placeholder="Email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                size="md"
+                className="w-full gap-x-2"
+                type="submit"
+                variant="portal-primary"
+                disabled={form.formState.isSubmitting}
+              >
+                {isLoading && (
+                  <span className="loading loading-spinner loading-md" />
+                )}
+                {submitText}
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </Fragment>
+    </AuthLayout>
+  )
+}
--- packages/pages/src/lib/auth/forgot-password.tsx
+++ packages/pages/src/lib/auth/forgot-password.tsx
@@ -1,106 +1,47 @@
 import * as z from 'zod'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
 import toast from 'react-hot-toast'
-import { api } from '@magickml/portal-api-client'
-import { AuthLayout, PortalLayout, MainLayout, BG } from '@magickml/portal-layout'
-import { IoIosArrowBack } from 'react-icons/io'
-import Link from 'next/link'
 import {
-  Label,
-  Form,
-  FormField,
-  FormItem,
-  FormControl,
-  Input,
-  FormMessage,
-  Button,
-  formInputStyles,
-} from '@magickml/portal-ui'
+  AuthFormLayout,
+  AuthLayout,
+  MainLayout,
+  PortalLayout,
+  BG,
+} from '@magickml/portal-layout'
+import { api } from '@magickml/portal-api-client'
 
 const emailFormSchema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
 })
 
 export const ForgotPassword = () => {
-  const form = useForm<z.infer<typeof emailFormSchema>>({
-    resolver: zodResolver(emailFormSchema),
-    defaultValues: { email: '' },
+  const {
+    mutateAsync: forgotPassword,
+    isLoading: forgotPasswordLoading,
+    status,
+  } = api.auth.forgotPassword.useMutation({
+    onSuccess: () => {
+      toast.success('Email sent successfully')
+    },
+    onError: error => {
+      toast.error(error.message)
+    },
   })
-  const { mutateAsync: forgotPassword, status } =
-    api.auth.forgotPassword.useMutation({
-      onSuccess: () => {
-        toast.success('Email sent successfully')
-      },
-      onError: error => {
-        toast.error(error.message)
-      },
-    })
 
   const handleSubmit = async (values: z.infer<typeof emailFormSchema>) => {
     const { email } = values
     await forgotPassword({ email })
   }
+
   return (
     <>
       {status === 'success' && <Modal />}
       {status !== 'success' && (
-
-          <div className="flex flex-col self-stretch w-full gap-3">
-            <div className="inline-flex items-center justify-start gap-2 dark:text-ds-primary-m text-ds-primary-p pb-6 text-sm font-medium">
-              <IoIosArrowBack className="mt-1" />
-              <Link
-                className="focus:underline hover:underline"
-                href="/auth/sign-in"
-              >
-                Back to Sign In
-              </Link>
-            </div>
-
-            <Label className="mb-2 text-3xl font-bold text-center text-black md:text-left dark:text-white font-montserrat">
-              Forgot Password?
-            </Label>
-            <p className="text-sm text-center font-medium font-montserrat pb-1">
-              Enter your email and we’ll send you a link to reset your password.
-            </p>
-
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(handleSubmit)}
-                className="w-full space-y-8"
-              >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          className={formInputStyles}
-                          placeholder="Email"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <Button
-                  size="md"
-                  className="w-full gap-x-2"
-                  type="submit"
-                  variant="portal-primary"
-                  disabled={form.formState.isSubmitting}
-                >
-                  {form.formState.isSubmitting && (
-                    <span className="loading loading-spinner loading-md" />
-                  )}
-                  {form.formState.isSubmitting ? 'Signing In...' : 'Sign In'}
-                </Button>
-              </form>
-            </Form>
-          </div>
+        <AuthFormLayout
+          title="Forgot Password?"
+          description="Enter your email and we'll send you a link to reset your password."
+          onSubmit={handleSubmit}
+          isLoading={forgotPasswordLoading}
+        />
       )}
     </>
   )
@@ -116,7 +57,6 @@ const Modal = () => {
     </AuthLayout>
   )
 }
-
 
 ForgotPassword.getLayout = function getLayout(page: any) {
   return (
