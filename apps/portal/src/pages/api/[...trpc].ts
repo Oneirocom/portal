1b8b54873e769d6bef1319bf0e832d0a3f89b1a1
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-07 17:19:16 -0500

    trpc openapi route

---
+++ apps/portal/src/pages/api/[...trpc].ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import cors from 'nextjs-cors'
+import { createOpenApiNextHandler } from 'trpc-openapi'
+import { portalRouter, createTRPCContext } from '@magickml/portal-api'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Setup CORS
+  await cors(req, res)
+
+  // Handle incoming OpenAPI requests
+  return createOpenApiNextHandler({
+    router: portalRouter as any,
+    createContext: createTRPCContext,
+    onError: error => {
+      console.error(error)
+    },
+  })(req, res)
+}
+
+export default handler
