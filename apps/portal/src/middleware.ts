87c084169bf2ff3670cc24b558839d5f34bf0220
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-10 05:54:40 -0500

    clerk middleware

---
+++ apps/portal/src/middleware.ts
@@ -0,0 +1,13 @@
+import { authMiddleware } from '@clerk/nextjs'
+
+export default authMiddleware({
+  afterAuth: (auth, req, evt) => {
+    // if (auth.userId || auth.isPublicRoute) {
+    // }
+  },
+  publicRoutes: ['/, /auth/sign-in, /auth/sign-up, /auth/verify', '/api'],
+})
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
