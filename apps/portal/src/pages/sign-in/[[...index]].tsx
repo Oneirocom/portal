f41b65d63dcf56a7c25f284b60a5ea8deb2f4cf6
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-10 00:27:29 -0500

    clerk sign in page

---
+++ apps/portal/src/pages/sign-in/[[...index]].tsx
@@ -0,0 +1,15 @@
+import { SignIn } from '@clerk/nextjs'
+import { MainLayout, PortalLayout } from '@magickml/portal-layout'
+import { clerkAppearance } from '../../styles/clerk'
+
+export default function Page() {
+  return <SignIn signUpUrl='/sign-up' />
+}
+
+Page.getLayout = function getLayout(page: any) {
+  return (
+    <PortalLayout>
+      <MainLayout classNames="items-center justify-center">{page}</MainLayout>
+    </PortalLayout>
+  )
+}
