2b8efdbdea0de6af0a065068d1eef96bea1f1449
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-10 23:24:16 -0500

    clerk styles for other parts of auth flows

--- apps/portal/src/styles/clerk.ts
+++ apps/portal/src/styles/clerk.ts
@@ -1,11 +1,6 @@
 import { type ClerkOptions, type Theme } from '@clerk/types'
 import { buttonVariants, formInputStyles } from '@magickml/portal-core-ui'
 
-const secondaryButton = buttonVariants({
-  variant: 'basic',
-  size: 'md',
-  className: 'shadow-none font-montAlt',
-})
 const commonText = 'text-ds-black dark:text-ds-white font-montserrat '
 
 const commmonAltText =
@@ -16,22 +11,24 @@ const headerTitle =
 
 const authTheme: Theme = {
   elements: {
-    headerTitle: 'text-center' + headerTitle,
-    headerSubtitle: 'hidden',
+    headerTitle: headerTitle,
+    headerSubtitle: 'text-ds-neutral font-montserrat',
     card: 'bg-ds-card rounded',
     formButtonPrimary:
-      // 'bg-ds-primary text-ds-black rounded-lg font-montserrat font-bold uppercase',
       buttonVariants({
         variant: 'portal-primary',
         size: 'sm',
-        className: 'font-bold hover:bg-ds-primary',
+        className: 'font-bold hover:bg-ds-primary font-montserrat',
       }),
-
     socialButtonsBlockButton: buttonVariants({
       variant: 'basic',
       size: 'sm',
       className: 'font-bold',
     }),
+    socialButtonsBlockButtonText: "text-ds-white font-montserrat font-semibold",
+    socialButtonsBlockButtonArrow: "text-ds-primary",
+    formHeaderSubtitle: 'text-ds-neutral font-montserrat',
+    formHeader__error: 'text-ds-error font-montserrat',
     formFieldInput: formInputStyles,
     formFieldLabel: commonText + 'font-semibold',
     formFieldAction: commmonAltText,
@@ -41,7 +38,20 @@ const authTheme: Theme = {
     selectOptionsContainer: 'bg-ds-card rounded text-white',
     selectOptionsContainer__role: 'text-red-500 bg-red-500',
     logoBox: 'hidden',
-    profileSectionContent__activeDevices: "h-full"
+    profileSectionContent__activeDevices: 'h-full',
+    dividerText: 'text-ds-neutral',
+    formHeaderTitle: commonText,
+    identityPreviewText: commonText,
+    formResendCodeLink: commmonAltText,
+    headerBackIcon: 'text-ds-primary   font-montserrat',
+    headerBackLink: 'text-ds-primary font-montserrat',
+    alternativeMethodsBlockButton: buttonVariants({
+      variant: 'portal-primary',
+      size: 'sm',
+      className: 'hover:bg-ds-primary',
+    }),
+    alternativeMethodsBlockButtonText: 'font-montserrat font-semibold',
+    footerPagesLink__help: 'text-ds-neutral font-montserrat',
   },
 }
 
@@ -57,18 +67,21 @@ export const clerkAppearance: ClerkOptions['appearance'] = {
       headerTitle,
       headerSubtitle:
         'text-ds-primary-p dark:text-ds-primary-m font-montserrat font-semibold',
-      accordionTriggerButton: "text-ds-black dark:text-ds-white shadow-none  font-montserrat font-semibold",
-      profileSectionPrimaryButton: "bg-ds-neutral text-ds-white font-montserrat font-semibold",
-      navbarButton: "bg-ds-neutral active:bg-ds-primary data-[active=true]:bg-ds-primary mt-2 text-ds-black font-montserrat font-semibold shadow-none",
-      navbarButtons__active: "bg-ds-primary",
-        badge: "bg-ds-primary text-ds-black font-montserrat font-medium text-xs",
-        profileSectionTitleText: commonText,
+      accordionTriggerButton:
+        'text-ds-black dark:text-ds-white shadow-none  font-montserrat font-semibold',
+      profileSectionPrimaryButton:
+        'bg-ds-neutral text-ds-white font-montserrat font-semibold',
+      navbarButton:
+        'bg-ds-neutral active:bg-ds-primary data-[active=true]:bg-ds-primary mt-2 text-ds-black font-montserrat font-semibold shadow-none',
+      navbarButtons__active: 'bg-ds-primary',
+      badge: 'bg-ds-primary text-ds-black font-montserrat font-medium text-xs',
+      profileSectionTitleText: commonText,
       profileSectionContent: commonText,
       // profileSectionContent__activeDevices: "h-40",
       // activeDeviceListItem: "focus:outline-none",
       // activeDeviceListItem__current: "outline outline-ds-primary",
       // accordionTriggerButton: "focus:outline-none focus:ring-0 focus:border-0",
     },
-  
   },
+  
 }
