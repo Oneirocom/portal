e1bc553d6436f10a73619a22aca2dc1053fbddb6
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-11 06:30:14 -0500

    remove ssr from projects page

--- apps/portal/src/pages/projects/index.tsx
+++ apps/portal/src/pages/projects/index.tsx
@@ -1,31 +1,3 @@
-import { authOptions } from '@magickml/portal-auth'
-import { GetServerSidePropsContext } from 'next'
-import { getServerSession } from 'next-auth'
 import { ProjectsPage } from '@magickml/portal-pages'
-import { unauthorizedRedirect } from '@magickml/portal-utils-server'
 
 export default ProjectsPage
-
-export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-  const session = await getServerSession(ctx.req, ctx.res, authOptions)
-
-  if (!session) {
-    return unauthorizedRedirect({
-      destination: '/auth/sign-in',
-      message: 'notLoggedIn',
-    })
-  }
-
-  // if (session?.user.role !== 'ADMIN') {
-  //   return {
-  //     redirect: {
-  //       destination: '/',
-  //       permanent: false,
-  //     },
-  //   }
-  // }
-
-  return {
-    props: {},
-  }
-}
