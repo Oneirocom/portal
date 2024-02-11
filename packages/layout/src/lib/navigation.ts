76c3e7c329df7904ff820accd3b3b04ed8842329
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-11 00:04:16 -0500

    cleanup navigation config

--- packages/layout/src/lib/navigation.ts
+++ packages/layout/src/lib/navigation.ts
@@ -3,10 +3,8 @@ import {
   ExploreIcon,
   HomeIcon,
   ProjectIcon,
-  SettingsIcon,
 } from '@magickml/portal-ui'
 import { Role } from '@magickml/portal-config'
-import posthog from 'posthog-js'
 
 export type Route = {
   name: string
@@ -15,43 +13,12 @@ export type Route = {
   enabled: boolean | string
 }
 
-let inviteEnabled: boolean | string = false
-
-const getInviteEnabledAsync = async () => {
-  return new Promise<boolean>(resolve => {
-    posthog.onFeatureFlags(function () {
-      if (posthog.isFeatureEnabled('use-invitations')) {
-        inviteEnabled = true
-      } else {
-        inviteEnabled = false
-      }
-      resolve(inviteEnabled)
-    })
-  })
-}
-
 // Role-based route configuration
 export const rolesConfig: Record<Role, string[]> = {
   ANONYMOUS: ['home'],
   USER: ['home', 'agents'],
-  TESTER: [
-    'home',
-    'agents',
-    'projects',
-    'workspace',
-    'interactions',
-    'knowledge',
-    'money',
-  ],
-  ADMIN: [
-    'home',
-    'agents',
-    'projects',
-    'workspace',
-    'interactions',
-    'knowledge',
-    'money',
-  ],
+  TESTER: ['home', 'agents', 'projects'],
+  ADMIN: ['home', 'agents', 'projects'],
 }
 
 export const allRoutes: Record<string, any> = {
@@ -66,14 +33,8 @@ export const allRoutes: Record<string, any> = {
     name: 'Explore',
     href: '/explore',
     icon: '/images/icons/community.svg',
-    enabled: true,
-    Iicon: ExploreIcon,
-  },
-  interactions: {
-    name: 'Interactions',
-    href: '/interactions',
-    icon: '/images/icons/interactions.svg',
     enabled: false,
+    Iicon: ExploreIcon,
   },
   agents: {
     name: 'Agents',
@@ -82,12 +43,6 @@ export const allRoutes: Record<string, any> = {
     enabled: true,
     Iicon: AgentIcon,
   },
-  knowledge: {
-    name: 'Knowledge',
-    href: '/knowledge',
-    icon: '/images/icons/knowledge.svg',
-    enabled: false,
-  },
   projects: {
     name: 'Projects',
     href: '/projects',
@@ -95,33 +50,11 @@ export const allRoutes: Record<string, any> = {
     enabled: true,
     Iicon: ProjectIcon,
   },
-  settings: {
-    name: 'Account',
-    href: '/account',
-    icon: '/images/icons/settings.svg',
-    enabled: true,
-    Iicon: SettingsIcon,
-  },
-
-  workspace: {
-    name: 'Workspace',
-    href: '/workspace',
-    icon: '/images/icons/mp.svg',
-    enabled: inviteEnabled,
-  },
-  money: {
-    name: 'Money',
-    href: '/money',
-    icon: '/images/icons/money.svg',
-    enabled: false,
-  },
 }
 
 // Function to generate navigation based on role
-export const getNavigationForRole = async (role: Role): Promise<Route[]> => {
-  await getInviteEnabledAsync() // Wait for inviteEnabled to be updated
-  return rolesConfig[role].map(routeKey => allRoutes[routeKey])
-}
+export const getNavigationForRole = (role: Role): Route[] =>
+  rolesConfig[role].map(routeKey => allRoutes[routeKey])
 
 export const secondaryNavigation = [
   {
@@ -163,30 +96,3 @@ export const secondaryNavigation = [
 export const filteredSecondaryNavigation = secondaryNavigation.filter(
   item => item.enabled
 )
-
-export const navigationV2 = [
-  {
-    name: 'Home',
-    href: '/',
-    icon: '/images/icons/home.svg',
-    enabled: true,
-    Iicon: HomeIcon,
-    admin: false,
-  },
-  {
-    name: 'Agents',
-    href: '/agents',
-    icon: '/images/icons/agents.svg',
-    enabled: true,
-    Iicon: AgentIcon,
-    admin: false,
-  },
-  {
-    name: 'Projects',
-    href: '/projects',
-    icon: '/images/icons/project-light.svg',
-    enabled: true,
-    Iicon: ProjectIcon,
-    admin: false,
-  },
-]
