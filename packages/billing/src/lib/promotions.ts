fbdca66732f9c938a2b771994c290eff7a03da37
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-14 17:26:56 -0500

    add promotions for wiz/apprentice

--- packages/billing/src/lib/promotions.ts
+++ packages/billing/src/lib/promotions.ts
@@ -17,3 +17,37 @@ export const makeTrialPromotion = async (userId: string) => {
     },
   })
 }
+
+export const makeApprenticePromotion = async (userId: string) => {
+  await prisma.promotion.create({
+    data: {
+      name: 'Apprentice',
+      description: 'Apprentice subscription credits.',
+      type: PromotionType.INTRO,
+      amount: new Decimal(10),
+      validFrom: new Date(),
+      validUntil: new Date(new Date().setDate(new Date().getDate() + 30)),
+      isUsed: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      userId,
+    },
+  })
+}
+
+export const makeWizardPromotion = async (userId: string) => {
+  await prisma.promotion.create({
+    data: {
+      name: 'Wizard',
+      description: 'Wizard subscription credits.',
+      type: PromotionType.INTRO,
+      amount: new Decimal(25),
+      validFrom: new Date(),
+      validUntil: new Date(new Date().setDate(new Date().getDate() + 30)),
+      isUsed: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      userId,
+    },
+  })
+}
