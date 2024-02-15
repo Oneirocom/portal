9921998c96b3be4210f30e6082b39089b36a79ae
Michael <m.sharpe@project89.org>
2024-02-15 15:38:54 -0800

    Refactor user.ts to handle error when stripeId is not found

--- packages/utils/server/src/lib/user.ts
+++ packages/utils/server/src/lib/user.ts
@@ -1,16 +1,26 @@
 import { clerkClient } from '@clerk/nextjs'
 
 export const getFullUser = async (userId: string) => {
+  console.log('GETTING USER', userId)
   const user = await clerkClient.users.getUser(userId)
+
+  console.log('USER FOUND', user)
+
   if (!user) {
     throw new Error('User not found')
   }
 
-  const customer = user.privateMetadata.stripeId as string
+  try {
+    const customer = user?.privateMetadata?.stripeId as string
 
-  if (!customer) {
-    throw new Error('Stripe customer not found')
+    if (!customer) {
+      throw new Error('Stripe customer not found')
+    }
+
+    return { user, customer }
+  } catch (error) {
+    console.log('error', error)
   }
 
-  return { user, customer }
+  return { user, customer: null }
 }
