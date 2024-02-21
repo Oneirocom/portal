81bdf82d73884db4e5fb470df1ba223f189c8994
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-21 18:26:05 -0500

    migration - add spells json array to templates

--- packages/db/src/lib/seed/index.ts
+++ packages/db/src/lib/seed/index.ts
@@ -1,21 +1,8 @@
 import { PrismaClient } from '../prisma/client-portal'
-import blankSpell from './templates/blank.spell.json'
 
 const prisma = new PrismaClient()
 
-async function main() {
-  // Create template
-  const template = await prisma.template.create({
-    data: {
-      name: 'BLANK AGENT',
-      description:
-        'Start a new agent from scratch. Choose this is you want to start a new project and adjust it in the editor from scratch.',
-      graph: blankSpell,
-    },
-  })
-
-  console.log({ template })
-}
+async function main() {}
 
 main()
   .then(async () => {
