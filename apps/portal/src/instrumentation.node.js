2912c980446c6a98e77505b8410964a177629542
Michael <m.sharpe@project89.org>
2024-02-20 22:02:33 -0800

    Fix instrumentation

--- apps/portal/src/instrumentation.node.js
+++ apps/portal/src/instrumentation.node.js
@@ -1,5 +1,6 @@
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+// import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base'
 import { Resource } from '@opentelemetry/resources'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
@@ -11,6 +12,7 @@ const sdk = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: SERVICE_NAME,
   }),
+  // spanProcessor: new SimpleSpanProcessor(new ConsoleSpanExporter()),
   spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
   instrumentations: [
     getNodeAutoInstrumentations({
