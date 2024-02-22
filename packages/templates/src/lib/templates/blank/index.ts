047645d0e4a32c8b37de552c9b491cee51f78dee
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-22 18:54:59 -0500

    add constant ids to templates

--- packages/templates/src/lib/templates/blank/index.ts
+++ packages/templates/src/lib/templates/blank/index.ts
@@ -2,6 +2,7 @@ import { type Prisma } from '@magickml/portal-db'
 import blankSpell from './blank.spell.json'
 
 export const blankTemplate: Prisma.TemplateCreateInput = {
+  id: 'blank',
   name: 'Blank Template',
   description: 'A blank template to start with',
   spells: [JSON.stringify(blankSpell)],
