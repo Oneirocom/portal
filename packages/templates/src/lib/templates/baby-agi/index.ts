83f88c773d7a66a269f8c2a8259426eecf8044fe
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-22 18:02:34 -0500

    add constant ids to base templates

--- packages/templates/src/lib/templates/baby-agi/index.ts
+++ packages/templates/src/lib/templates/baby-agi/index.ts
@@ -2,6 +2,7 @@ import { type Prisma } from '@magickml/portal-db'
 import babeAGISpell from './baby-agi.spell.json'
 
 export const babyAGITemplate: Prisma.TemplateCreateInput = {
+  id: 'baby-agi',
   name: 'Baby AGI',
   description: 'A template for a baby AGI',
   spells: [JSON.stringify(babeAGISpell)],
--- packages/templates/src/lib/templates/slack-discord/index.ts
+++ packages/templates/src/lib/templates/slack-discord/index.ts
@@ -3,6 +3,7 @@ import slackDiscordSpell from './slack.spell.json'
 import discordSpell from './discord.spell.json'
 
 export const slackDiscordTemplate: Prisma.TemplateCreateInput = {
+  id: 'slack-discord',
   name: 'Slack and Discord',
   description: 'A template setup for basic Slack and Discord messaging.',
   spells: [JSON.stringify(slackDiscordSpell), JSON.stringify(discordSpell)],
