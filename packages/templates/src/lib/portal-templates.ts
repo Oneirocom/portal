eb2b6f553d004dc62916e2397264bc43da44612e
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-21 19:20:51 -0500

    use tempalte package in portal api

--- packages/templates/src/lib/portal-templates.ts
+++ packages/templates/src/lib/portal-templates.ts
@@ -1,6 +1,10 @@
 import { type Prisma, prisma } from '@magickml/portal-db'
-
 import { blankTemplate, babyAGITemplate } from './templates'
+import { makeClient } from 'ideClient'
+import { ideServerUrl } from './constants'
+import { v4 as uuidv4 } from 'uuid'
+
+const app = makeClient(ideServerUrl)
 
 export const baseTemplates: Prisma.TemplateCreateInput[] = [
   blankTemplate,
@@ -42,4 +46,55 @@ export const createFromAgent = async (input: CreateFromAgentInput) => {
   }
 
   return prisma.template.createMany({ data: [template] })
+}
+
+interface CreateFromTemplateInput {
+  projectName: string
+  agentName: string
+  templateId: string
+  owner: string
+}
+export const createFromTemplate = async (input: CreateFromTemplateInput) => {
+  const { projectName, agentName, templateId, owner } = input
+  const template = await prisma.template.findUnique({
+    where: { id: templateId },
+  })
+
+  if (!template) {
+    throw new Error('Template not found')
+  }
+
+  const project = await prisma.project.create({
+    data: {
+      name: projectName,
+      description: `project for ${agentName}`,
+      owner,
+      updatedAt: new Date().toISOString(),
+      createdAt: new Date().toISOString(),
+    },
+  })
+
+  await app.service('projects').create({
+    name: projectName,
+    projectId: project.id,
+  })
+
+  const templateSpells = template.spells.map(spell => JSON.parse(spell))
+
+  for (const tspell of templateSpells) {
+    console.log('tspell', tspell.graph)
+    const input = {
+      id: uuidv4(),
+      projectId: project.id,
+      name: tspell?.name,
+      graph: tspell?.graph || {},
+      type: 'behave',
+    }
+
+    await app.service('spells').create(input)
+  }
+
+  return {
+    project: project.id,
+  }
 }
