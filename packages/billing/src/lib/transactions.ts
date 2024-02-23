b02f45a5d6bafb07a1ba98a7177872c990574d6b
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-02-23 16:39:51 -0500

    transaction utils file

---
+++ packages/billing/src/lib/transactions.ts
@@ -0,0 +1,19 @@
+import { prismaPortal, TransactionSource } from '@magickml/portal-db'
+
+export async function recordTransaction(
+  userId: string,
+  projectId: string,
+  cost: number,
+  success: boolean,
+  source: TransactionSource
+) {
+  await prismaPortal.transaction.create({
+    data: {
+      userId,
+      projectId,
+      cost,
+      success,
+      source,
+    },
+  })
+}
