7d400343bcc21001f42dd1efb0af723253ba4c50
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-22 14:33:17 -0400

    Add loading state to Editor page

--- packages/pages/src/lib/editor.tsx
+++ packages/pages/src/lib/editor.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react'
 import posthog from 'posthog-js'
-import { Maintenance } from '@magickml/portal-ui'
 import { type AppConfig } from '@magickml/providers'
 import dynamic from 'next/dynamic'
 import { useTheme } from 'next-themes'
+import { Maintenance } from '@magickml/portal-ui'
 
 const MagickIDE = dynamic(
   () => import('client/editor').then(mod => mod.MagickIDE),
-  { ssr: false }
+  {
+    ssr: false,
+  }
 )
 
 export const Editor = (props: AppConfig): React.ReactElement | null => {
+  const [loading, setLoading] = useState(true)
+  const [status, setStatus] = useState<string>('Initializing editor...')
   const [cookie, setCookie] = useState<boolean | null>(null)
   const { theme } = useTheme()
 
@@ -28,10 +32,20 @@ export const Editor = (props: AppConfig): React.ReactElement | null => {
 
   return (
     <>
+      {loading && (
+        <div className=" h-dvh flex items-center justify-center flex-col gap-y-1 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <span className="loading loading-spinner loading-lg text-white" />
+          <span className="text-sm font-montserrat font-medium tracking-tight">
+            {status}
+          </span>
+        </div>
+      )}
       {process.env.NEXT_PUBLIC_AIDE_MAINTENANCE_MODE === 'true' ? (
         <Maintenance mode="editor" />
       ) : cookie !== null ? (
-        <MagickIDE config={props} />
+        <>
+          <MagickIDE config={props} loading={[loading, setLoading]} />
+        </>
       ) : null}
     </>
   )
