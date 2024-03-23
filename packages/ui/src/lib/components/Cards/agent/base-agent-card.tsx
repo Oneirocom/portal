ce3f748b7a9c9139364a49fb0b55ee5763b25475
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-23 19:57:30 -0400

    Update BaseAgentCard component

--- packages/ui/src/lib/components/Cards/agent/base-agent-card.tsx
+++ packages/ui/src/lib/components/Cards/agent/base-agent-card.tsx
@@ -1,32 +1,37 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@magickml/client-ui'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@magickml/client-ui'
 import Image from 'next/legacy/image'
 import { ImageType, getImage } from '@magickml/portal-utils-shared'
 import { ReactNode } from 'react'
+import { AgentCardInfo } from './types'
 
 type BaseAgentCardProps = {
-  id: string
-  name: string | null
-  image: string | null
-  description: string | null
+  agent: AgentCardInfo
   onClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
   menu?: ReactNode
+  footer?: ReactNode
 }
 
 export const BaseAgentCard: React.FC<BaseAgentCardProps> = ({
-  id,
-  name,
-  image,
-  description,
+  agent,
   onClick,
   menu,
+  footer,
 }) => {
+  const { id, name, image, description } = agent
+
   return (
     <Card
       onClick={onClick}
       key={id}
-      className="w-44 h-60 lg:w-56 lg:h-80 flex flex-col hover:border-ds-primary hover:scale-105 transition-all duration-150 ease-in-out hover:overflow-visible cursor-pointer"
+      className="w-44 h-60 lg:w-56 lg:h-80 flex flex-col border-ds-neutral hover:border-ds-primary hover:scale-105 transition-all duration-150 ease-in-out hover:overflow-visible cursor-pointer"
     >
-      <div className="relative w-full basis-6/12 rounded-t-xl overflow-hidden m-0 p-0">
+      <div className="relative w-full h-[53%] rounded-t-xl overflow-hidden m-0 p-0">
         {menu}
         <Image
           src={getImage({
@@ -39,16 +44,17 @@ export const BaseAgentCard: React.FC<BaseAgentCardProps> = ({
           className="object-cover"
         />
       </div>
-      <CardHeader className="p-0 pt-2.5 text-center basis-5/12 text-pretty">
-        <CardTitle className="font-montserrat text-sm font-semibold">
+      <CardHeader className="p-0 pt-2.5 h-[35%] text-center text-pretty grow">
+        <CardTitle className="font-montserrat text-sm lg:text-base font-medium">
           {name}
         </CardTitle>
         {description && (
-          <CardDescription className="mx-1 line-clamp-3 lg:line-clamp-4">
+          <CardDescription className="mx-2 line-clamp-2 text-xs lg:text-base text-center dark:!text-ds-white !text-ds-black">
             {description}
           </CardDescription>
         )}
       </CardHeader>
+      <CardFooter className="h-[12%]">{footer}</CardFooter>
     </Card>
   )
 }
