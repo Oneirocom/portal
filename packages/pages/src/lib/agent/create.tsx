269395ee614dc645abe141613c4d42b4678d6f24
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-03-23 20:51:00 -0400

    Refactor agent creation and template handling

--- packages/pages/src/lib/agent/create.tsx
+++ packages/pages/src/lib/agent/create.tsx
@@ -16,33 +16,6 @@ export const CreateAgentPage = () => {
   const { data: templates, isLoading: templatesLoading } =
     api.agents.getTemplates.useQuery()
 
-  const { mutateAsync: create, isLoading: isMakingAgentRemix } =
-    api.agents.createFromTemplate.useMutation({
-      onSuccess: agent => {
-        setOpen(false)
-        setName('')
-        window.open(`/projects/${agent.project}`, '_blank')
-        setTemplateId('')
-      },
-      onError: error => {
-        setOpen(false)
-        setName('')
-        setTemplateId('')
-        toast.error(error.message)
-      },
-    })
-
-  const handleRemixAgent = async () => {
-    if (!templateId) return
-    await create({ name, templateId })
-  }
-
-  const onTemplateClick = (templateId: string | null) => {
-    if (!templateId) return
-    setOpen(true)
-    setTemplateId(templateId)
-  }
-
   return (
     <>
       <Head>
@@ -65,31 +38,6 @@ export const CreateAgentPage = () => {
             />
           ))}
       </div>
-      <MagickDialog
-        logo={false}
-        open={open}
-        hideButton={true}
-        isLoading={isMakingAgentRemix}
-        setOpen={setOpen}
-        trigger={<RemixButton onClick={() => setOpen(true)} />}
-        title="Name Your Agent"
-        description="Using a Template will create a new Agent with the same spell as the Template."
-        onSubmit={handleRemixAgent}
-        submitText="Create"
-        submitDisabled={!name}
-      >
-        <Label htmlFor="name" className="text-left">
-          Name your agent
-        </Label>
-        <Input
-          id="agentName"
-          autoComplete="off"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          className="focus:border-secondary-highlight bg-ds-card-alt placeholder:font-montserrat placeholder:text-black/70 dark:placeholder:text-white/70 w-full p-2 border-2 border-[#808f9a] rounded-[8px] dark:text-white"
-          placeholder="Tom Bombadil"
-        />
-      </MagickDialog>
     </>
   )
 }
@@ -99,18 +47,5 @@ CreateAgentPage.getLayout = (page: React.ReactElement) => {
     <PortalLayout>
       <MainLayout classNames="p-2 lg:p-10 gap-y-4">{page}</MainLayout>
     </PortalLayout>
-  )
-}
-
-type RemixButtonProps = {
-  onClick: () => void
-}
-
-const RemixButton: FunctionComponent<RemixButtonProps> = ({ onClick }) => {
-  return (
-    <Button onClick={onClick} variant="agent" size="sm">
-      <SlRefresh className="text-lg lg:mr-1" />
-      <span className="hidden lg:flex">Remix</span>
-    </Button>
   )
 }
--- packages/ui/src/lib/components/Cards/agent/agent-card-template.tsx
+++ packages/ui/src/lib/components/Cards/agent/agent-card-template.tsx
@@ -2,20 +2,37 @@ import { useRef, useState } from 'react'
 import { BaseAgentCard } from './base-agent-card'
 import { AgentCardFooter } from './agent-card-footer'
 import { AgentCardInfo } from './types'
+import React from 'react'
+import { Button, Input, Label, MagickDialog } from '@magickml/client-ui'
+import { SlRefresh } from 'react-icons/sl'
+import { api } from '@magickml/portal-api-client'
+import toast from 'react-hot-toast'
 
 type AgentCardTemplateProps = AgentCardInfo & {}
 
 export const AgentCardTemplate: React.FC<AgentCardTemplateProps> = agent => {
   const footerState = useState(false)
   const footerRef = useRef<HTMLButtonElement>(null)
+  const createState = useState(false)
+
+  const isFooterActive = (e: MouseEvent) => {
+    if (footerRef.current) {
+      return footerRef.current.contains(e.target as Node)
+    }
+    return false
+  }
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (footerState[0]) {
+    if (isFooterActive(e.nativeEvent) || footerState[0]) {
       e.stopPropagation()
-    } else {
-      e.preventDefault()
-      footerState[1](true)
+    } else if (!createState[0]) {
+      createState[1](true)
     }
+  }
+
+  const handleFooterSubmit = () => {
+    footerState[1](false)
+    createState[1](true)
   }
 
   return (
@@ -23,14 +40,89 @@ export const AgentCardTemplate: React.FC<AgentCardTemplateProps> = agent => {
       agent={agent}
       onClick={handleClick}
       footer={
-        <AgentCardFooter
-          agent={agent}
-          state={footerState}
-          buttonRef={footerRef}
-          submitText="Build"
-          onSubmit={() => console.log('More info')}
-        />
+        <>
+          <AgentCardFooter
+            agent={agent}
+            state={footerState}
+            buttonRef={footerRef}
+            submitText="Build"
+            onSubmit={handleFooterSubmit}
+          />
+          <CreateDialog templateId={agent.id} state={createState} />
+        </>
       }
     />
+  )
+}
+
+type CreateDialogProps = {
+  state: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
+  templateId: string
+}
+
+const CreateDialog: React.FC<CreateDialogProps> = ({ state, templateId }) => {
+  const [open, setOpen] = state
+  const [name, setName] = useState('')
+
+  const { mutateAsync: create, isLoading } =
+    api.agents.createFromTemplate.useMutation({
+      onSuccess: agent => {
+        setOpen(false)
+        setName('')
+        window.open(`/projects/${agent.project}`, '_blank')
+      },
+      onError: error => {
+        setOpen(false)
+        setName('')
+        toast.error(error.message)
+      },
+    })
+
+  const handleCreate = async () => {
+    if (!name) {
+      toast.error('Please enter a name for your agent.')
+      return
+    }
+    await create({ templateId, name })
+  }
+
+  return (
+    <MagickDialog
+      logo={false}
+      open={open}
+      hideButton={true}
+      isLoading={isLoading}
+      setOpen={setOpen}
+      title="Name Your Agent"
+      description="Using a Template will create a new Agent with the same spell as the Template."
+      onSubmit={handleCreate}
+      submitText="Create"
+      submitDisabled={!name}
+    >
+      <Label htmlFor="name" className="text-left">
+        Name your agent
+      </Label>
+      <Input
+        id="agentName"
+        autoComplete="off"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        className="focus:border-secondary-highlight bg-ds-card-alt placeholder:font-montserrat placeholder:text-black/70 dark:placeholder:text-white/70 w-full p-2 border-2 border-[#808f9a] rounded-[8px] dark:text-white"
+        placeholder="Tom Bombadil"
+      />
+    </MagickDialog>
+  )
+}
+
+type RemixButtonProps = {
+  onClick: () => void
+}
+
+const RemixButton: React.FC<RemixButtonProps> = ({ onClick }) => {
+  return (
+    <Button onClick={onClick} variant="agent" size="sm">
+      <SlRefresh className="text-lg lg:mr-1" />
+      <span className="hidden lg:flex">Remix</span>
+    </Button>
   )
 }
