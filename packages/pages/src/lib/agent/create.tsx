a0ed4f32c2aa55c46c52b6de14a02c7c12934f56
Jakob <haruhunab1320@gmail.com>
2024-03-26 13:45:06 -0700

    handle remix on enter

--- packages/pages/src/lib/agent/create.tsx
+++ packages/pages/src/lib/agent/create.tsx
@@ -73,48 +73,37 @@ export const CreateAgentPage = () => {
               version={t.templateVersions[0]?.version.toString() ?? null}
             />
           ))}
-      </PageSection>
-
-      <PageSection
-        title="Community Agent Templates"
-        loading={communityQuery.isLoading}
+      </div>
+      <MagickDialog
+        logo={false}
+        open={open}
+        hideButton={true}
+        isLoading={isMakingAgentRemix}
+        setOpen={setOpen}
+        trigger={<RemixButton onClick={() => setOpen(true)} />}
+        title="Name Your Agent"
+        description="Using a Template will create a new Agent with the same spell as the Template."
+        onSubmit={handleRemixAgent}
+        submitText="Create"
+        submitDisabled={!name}
       >
-        {!communityQuery.isLoading &&
-          communityTemplates &&
-          communityTemplates.map((t, i: number) => (
-            <AgentCardTemplate
-              key={`${t.id}--community`}
-              id={t.id}
-              name={t.name ?? 'Untitled'}
-              image={t.image}
-              description={t?.description ?? ''}
-              isPublic={t.public}
-              creator={t.userId}
-              metadata={t.templateVersions[0]?.metadata ?? null}
-              ogAgentId={t.ogAgentId}
-              version={t.templateVersions[0]?.version.toString() ?? null}
-            />
-          ))}
-      </PageSection>
-
-      <PageSection title="Your Agent Templates" loading={userQuery.isLoading}>
-        {!userQuery.isLoading &&
-          userTemplates &&
-          userTemplates.map((t, i: number) => (
-            <AgentCardTemplate
-              key={`${t.id}--user`}
-              id={t.id}
-              name={t.name ?? 'Untitled'}
-              image={t.image}
-              description={t?.description ?? ''}
-              isPublic={t.public}
-              creator={t.userId}
-              metadata={t.templateVersions[0]?.metadata ?? null}
-              ogAgentId={t.ogAgentId}
-              version={t.templateVersions[0]?.version.toString() ?? null}
-            />
-          ))}
-      </PageSection>
+        <Label htmlFor="name" className="text-left">
+          Name your agent
+        </Label>
+        <Input
+          id="agentName"
+          autoComplete="off"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          className="focus:border-secondary-highlight bg-ds-card-alt placeholder:font-montserrat placeholder:text-black/70 dark:placeholder:text-white/70 w-full p-2 border-2 border-[#808f9a] rounded-[8px] dark:text-white"
+          placeholder="Tom Bombadil"
+          onKeyDown={e => {
+            if (e.key === 'Enter') {
+              handleRemixAgent()
+            }
+          }}
+        />
+      </MagickDialog>
     </>
   )
 }
