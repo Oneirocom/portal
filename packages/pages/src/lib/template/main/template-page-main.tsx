d0ecef6df1f9bd6fd22a105e2b82dcd1d177cde0
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-05 18:04:28 -0400

    use readonly flow in template-page-main component

--- packages/pages/src/lib/template/main/template-page-main.tsx
+++ packages/pages/src/lib/template/main/template-page-main.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { SpellInterfaceWithGraph } from 'server/schemas'
 import { TemplateGetStaticProps } from '../template-page-isr'
 import { TemplatePageHeader } from './template-page-header'
 import { TemplatePageSubHeader } from './template-page-subheader'
 import dynamic from 'next/dynamic'
+import { useRef } from 'react'
 
 type TemplatePageMainProps = {
   template: TemplateGetStaticProps['template']
@@ -10,11 +11,14 @@ type TemplatePageMainProps = {
 
 export const TemplatePageMain = (props: TemplatePageMainProps) => {
   const { template } = props
-  const spell = template?.templateVersions?.[0].spells?.[0]
-  const [loading, setLoading] = useState(true)
+  const spell = template?.templateVersions?.[0].spells?.[0] as
+    | SpellInterfaceWithGraph
+    | undefined
+    | null
+  const parentRef = useRef<HTMLDivElement>(null)
 
   const ReadOnlyFlow = dynamic(
-    () => import('client/editor').then(mod => mod.MagickIDE),
+    () => import('@magickml/flow-core').then(mod => mod.ReadOnlyFlow),
     {
       ssr: false,
       loading: () => (
@@ -32,9 +36,21 @@ export const TemplatePageMain = (props: TemplatePageMainProps) => {
     <main className="w-full px-10 items-start gap-8 flex flex-col">
       <TemplatePageHeader template={template} />
       <TemplatePageSubHeader template={template} />
-      <div className="w-full h-full max-h-[400px] max-w-7xl bg-ds-card-alt rounded-[5px] border border-ds-primary-m dark:ds-primary-p overflow-hidden">
-        {/* @ts-ignore */}
-        <ReadOnlyFlow spell={spell} />
+      <div
+        ref={parentRef}
+        className="w-full h-full max-h-[400px] max-w-7xl bg-ds-card-alt rounded-[5px] border border-ds-primary-m dark:ds-primary-p overflow-hidden"
+      >
+        {spell ? (
+          <ReadOnlyFlow
+            spell={spell}
+            windowDimensions={{ width: 1280, height: 400 }}
+            parentRef={parentRef}
+          />
+        ) : (
+          <div className="flex items-center justify-center w-full h-full text-ds-primary-m dark:text-ds-primary-p">
+            No spell found
+          </div>
+        )}
       </div>
     </main>
   )
