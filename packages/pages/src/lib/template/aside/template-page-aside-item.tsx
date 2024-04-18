a10b9758d37a57d1b6b2605b5c9d3a51887d05d4
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-18 18:08:42 -0400

    Refactor TemplatePageAsideItem component to fit new figma design

--- packages/pages/src/lib/template/aside/template-page-aside-item.tsx
+++ packages/pages/src/lib/template/aside/template-page-aside-item.tsx
@@ -1,17 +1,30 @@
-import React, { PropsWithChildren } from 'react'
-import { TemplateGetStaticProps } from '../template-page-isr'
+import { cn } from '@magickml/client-ui'
+import type { SvgIconTypeMap } from '@mui/material'
+import type { OverridableComponent } from '@mui/material/OverridableComponent'
 
-type TemplatePageAsideItemProps = {
+interface TemplatePageAsideItemProps {
   title: string
+  children: React.ReactNode
+  noBorder?: boolean
+  Icon?:
+    | (OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & { muiName: string })
+    | React.FC<React.SVGProps<SVGSVGElement>>
 }
 
-export const TemplatePageAsideItem = (
-  props: PropsWithChildren<TemplatePageAsideItemProps>
-) => {
+export const TemplatePageAsideItem = (props: TemplatePageAsideItemProps) => {
+  const { title, children, Icon } = props
   return (
-    <div className="flex flex-col">
-      <p className='font-normal text-2xl'>{props.title}</p>
-      <div className="flex flex-col pt-1 text-base">{props.children}</div>
+    <div
+      className={cn(
+        'inline-flex justify-between items-center w-full h-fit',
+        props.noBorder ? '' : 'border-b border-zinc-600'
+      )}
+    >
+      <p className="inline-flex gap-x-1 text-neutral-400  font-semibold font-['Montserrat']">
+        {Icon && <Icon />} {title}
+      </p>
+
+      <div className="w-1/2">{children}</div>
     </div>
   )
 }
