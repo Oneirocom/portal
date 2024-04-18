ed67c5593e91ae15f3bc352b3701ba175bd22c53
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-18 18:40:41 -0400

    Refactor spell list rendering and add preview section

--- packages/pages/src/lib/template/main/template-page-main.tsx
+++ packages/pages/src/lib/template/main/template-page-main.tsx
@@ -43,46 +43,50 @@ export const TemplatePageMain = (props: TemplatePageMainProps) => {
       <TemplatePageHeader template={template} />
 
       <TemplatePageSubHeader template={template} />
-      {/* List spell names */}
 
-      <div
-        ref={parentRef}
-        className="w-full h-full max-h-[400px] max-w-7xl bg-ds-card-alt rounded-[5px] border border-ds-primary-m dark:ds-primary-p overflow-hidden"
-      >
-        {spells && (
-          <div className="overflow-hidden border-ds-neutral inline-flex items-center justify-start w-full bg-ds-card h-6 border-b">
-            <span className="flex items-center border-r border-r-ds-neutral overflow-hidden justify-start max-w-fit h-12 px-4 font-medium">
-              <p className="text-ellipsis text-nowrap">Spells</p>
-            </span>
-            {spells.map(spell => (
-              <span
-                key={spell.id}
-                className={cn(
-                  'flex items-center border-r font-light border-r-ds-neutral overflow-hidden justify-start max-w-fit h-12 px-4 text-ds-neutral',
-                  {
-                    'text-ds-primary': activeSpell?.id === spell.id,
-                    '': activeSpell?.id === spell.id,
-                  }
-                )}
-                onClick={() => setActiveSpell(spell)}
-              >
-                <p className="text-ellipsis text-nowrap">{spell.name}</p>
+      <div className="flex flex-col gap-y-4 w-full full min-h-[500px] max-h-[700px] bg-ds-card-alt p-4 rounded-lg">
+        <h2 className="text-2xl font-normal">Preview</h2>
+
+        <div
+          ref={parentRef}
+          className="w-full h-full max-h-[400px] max-w-7xl bg-ds-card-alt rounded-[5px] border border-ds-primary-m dark:ds-primary-p overflow-hidden"
+          // className="flex flex-col gap-y-4 w-full bg-ds-card-alt p-4 rounded-lg">
+        >
+          {spells && (
+            <div className="overflow-hidden border-ds-neutral inline-flex items-center justify-start w-full bg-ds-card h-6 border-b">
+              <span className="flex items-center border-r border-r-ds-neutral overflow-hidden justify-start max-w-fit h-12 px-4 font-medium">
+                <p className="text-ellipsis text-nowrap">Spells</p>
               </span>
-            ))}
-          </div>
-        )}
+              {spells.map(spell => (
+                <span
+                  key={spell.id}
+                  className={cn(
+                    'flex items-center border-r font-light border-r-ds-neutral overflow-hidden justify-start max-w-fit h-12 px-4 text-ds-neutral',
+                    {
+                      'text-ds-primary-p dark:text-ds-primary-m': activeSpell?.id === spell.id,
+                      '': activeSpell?.id === spell.id,
+                    }
+                  )}
+                  onClick={() => setActiveSpell(spell)}
+                >
+                  <p className="text-ellipsis text-nowrap">{spell.name}</p>
+                </span>
+              ))}
+            </div>
+          )}
 
-        {activeSpell ? (
-          <ReadOnlyFlow
-            spell={activeSpell}
-            windowDimensions={{ width: 1280, height: 400 }}
-            parentRef={parentRef}
-          />
-        ) : (
-          <div className="flex items-center justify-center w-full h-full text-ds-primary-m dark:text-ds-primary-p">
-            No spell found
-          </div>
-        )}
+          {activeSpell ? (
+            <ReadOnlyFlow
+              spell={activeSpell}
+              windowDimensions={{ width: 1280, height: 400 }}
+              parentRef={parentRef}
+            />
+          ) : (
+            <div className="flex items-center justify-center w-full h-full text-ds-primary-m dark:text-ds-primary-p">
+              No spell found
+            </div>
+          )}
+        </div>
       </div>
     </main>
   )
