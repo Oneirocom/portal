99af53d3e35239b86958216aac2904442c3364cd
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-18 12:48:12 -0400

    updaet template page path to match route

---
+++ apps/portal/src/pages/templates/[templateId]/index.tsx
@@ -0,0 +1,11 @@
+import {
+  TemplatePage,
+  templatesGetStaticProps,
+  templatesGetStaticPaths,
+} from '@magickml/portal-pages'
+
+export default TemplatePage
+
+export { templatesGetStaticProps as getStaticProps }
+
+export { templatesGetStaticPaths as getStaticPaths }
--- apps/portal/src/pages/templates/index.tsx
+++
@@ -1,5 +0,0 @@
-import { TemplatePage, templatesGetStaticProps } from '@magickml/portal-pages'
-
-export default TemplatePage
-
-export { templatesGetStaticProps as getStaticProps }
