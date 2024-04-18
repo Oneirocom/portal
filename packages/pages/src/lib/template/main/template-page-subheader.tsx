7cd0653a6841265bbb59b709d09a430d02615c51
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-18 18:40:12 -0400

    Add background color and padding to template page subheader

--- packages/pages/src/lib/template/main/template-page-subheader.tsx
+++ packages/pages/src/lib/template/main/template-page-subheader.tsx
@@ -7,7 +7,7 @@ type TemplatePageSubHeaderProps = {
 export const TemplatePageSubHeader = (props: TemplatePageSubHeaderProps) => {
   const { template } = props
   return (
-      <div className="flex flex-col gap-y-4 w-full">
+      <div className="flex flex-col gap-y-4 w-full bg-ds-card-alt p-4 rounded-lg">
         <h2 className="text-2xl font-normal">Description</h2>
         <p className="text-base font-normal opacity-90">
           {template?.description || 'No description provided'}
