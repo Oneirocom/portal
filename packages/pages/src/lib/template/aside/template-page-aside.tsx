2e83de759778b449ffb0f01ea0ab0333da4daefc
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-18 18:08:47 -0400

    Refactor template-page-aside component

--- packages/pages/src/lib/template/aside/template-page-aside.tsx
+++ packages/pages/src/lib/template/aside/template-page-aside.tsx
@@ -1,10 +1,11 @@
 import { ImageType } from '@magickml/portal-utils-shared'
-import Image from 'next/image'
 import { PropsWithChildren } from 'react'
 import { getImage } from 'shared/utils'
 import type { TemplateGetStaticProps } from '../template-page-isr'
-import { Avatar, AvatarImage } from '@magickml/client-ui'
+import { Avatar, AvatarImage, cn } from '@magickml/client-ui'
 import { TemplatePageAsideItem } from './template-page-aside-item'
+import { WizardHatIcon, MagickWandIcon, CrystalBallIcon } from '../icons'
+import { Psychology, Update } from '@mui/icons-material'
 
 type TemplatePageSideProps = {
   template: TemplateGetStaticProps['template']
@@ -21,62 +22,72 @@ export const TemplatePageSide = (
   const models = (metadata?.models as string[]) ?? []
 
   return (
-    <aside className="px-5 w-96 pb-4 flex-col justify-center items-start gap-y-10 hidden md:inline-flex">
-      <Image
-        src={getImage({
-          id: template?.id ?? '1',
-          image: template?.image ?? '',
-          type: ImageType.IMAGE,
-        })}
-        alt={template?.name ?? 'template-image'}
-        className="bg-ds-card-alt rounded-[5px] overflow-hidden"
-        width={300}
-        height={300}
-      />
+    <aside className="px-5 min-w-96 flex-col justify-start items-start hidden md:inline-flex gap-10">
+      <Avatar className="w-48 h-48 mx-auto">
+        <AvatarImage
+          className="h-full w-full rounded-[10px]"
+          src={getImage({
+            id: template?.id ?? '1',
+            image: template?.image ?? '',
+            type: ImageType.IMAGE,
+          })}
+          alt={template.name?.at(0) || 'A'}
+        />
+        {template?.image ? template.name?.at(0) || 'A' : null}
+      </Avatar>
 
-      <div className="self-stretch flex flex-col justify-start items-start gap-8">
-        <TemplatePageAsideItem title="Built By">
-          <div className="inline-flex items-center gap-x-1 justify-start">
-            <Avatar>
+      <div className="inline-flex flex-col w-full gap-5">
+        <TemplatePageAsideItem title="Built By" Icon={WizardHatIcon}>
+          <div className="inline-flex w-full items-center justify-start gap-x-1">
+            <Avatar className="w-8 h-8 items-start justify-start">
               <AvatarImage
-                className=" w-full h-full rounded-full"
+                className="h-full w-full rounded-full"
+                alt="Magick"
                 src={getImage({
                   id: template?.id ?? '1',
                   image: template?.image ?? '',
                   type: ImageType.IMAGE,
                 })}
-                alt={template.name?.at(0) || 'A'}
               />
-              {template?.image ? template.name?.at(0) || 'A' : null}
             </Avatar>
-            <p className="text-ds-primary font-medium text-base">@username</p>
+            <span className="text-ds-primary-p dark:text-ds-primary-m  font-medium">
+              Magick
+            </span>
           </div>
         </TemplatePageAsideItem>
 
-        <TemplatePageAsideItem title="Models Used">
-          {models.map(model => (
-            <p key={model} className="font-medium text-base">
-              {model}
-            </p>
-          ))}
+        {/* <TemplatePageAsideItem title="Interactions">
+          <p className="text-white text-base font-medium">93</p>
         </TemplatePageAsideItem>
 
-        <TemplatePageAsideItem title="Current Version">
-          <p className="text-base font-normal">
-            {template.templateVersions?.[0].version ?? '1'}
+        <TemplatePageAsideItem title="Likes">
+          <p className="text-white text-base font-medium">7</p>
+        </TemplatePageAsideItem> */}
+
+        <TemplatePageAsideItem title="Current Version" Icon={Psychology}>
+          <p className="text-white  font-medium">
+            {`Version ${template?.templateVersions?.[0].version ?? '1'}`}
           </p>
         </TemplatePageAsideItem>
 
-        <TemplatePageAsideItem title="Created">
-          <p className="text-base font-normal">
+        <TemplatePageAsideItem title="Created" Icon={MagickWandIcon}>
+          <p className="text-white  font-medium">
             {new Date(template?.createdAt ?? '').toLocaleDateString()}
           </p>
         </TemplatePageAsideItem>
 
-        <TemplatePageAsideItem title="Last Updated">
-          <p className="text-base font-normal">
+        <TemplatePageAsideItem title="Last Updated" Icon={Update}>
+          <p className="text-white  font-medium">
             {new Date(template?.updatedAt ?? '').toLocaleDateString()}
           </p>
+        </TemplatePageAsideItem>
+
+        <TemplatePageAsideItem title="Models Used" Icon={CrystalBallIcon}>
+          {models.map(model => (
+            <p key={model} className="text-gray-100  font-normal">
+              {model}
+            </p>
+          ))}
         </TemplatePageAsideItem>
       </div>
     </aside>
