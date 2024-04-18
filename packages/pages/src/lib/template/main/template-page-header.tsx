4b93c8fe2d18db21208dbb08b7571faec866e546
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-04-18 12:46:58 -0400

    Add router and create agent dialog to TemplatePageHeader component

--- packages/pages/src/lib/template/main/template-page-header.tsx
+++ packages/pages/src/lib/template/main/template-page-header.tsx
@@ -1,5 +1,8 @@
+import { useRouter } from 'next/router'
 import { TemplateGetStaticProps } from '../template-page-isr'
 import { Button } from '@magickml/client-ui'
+import { CreateAgentDialog } from '@magickml/portal-ui'
+import { useState } from 'react'
 
 type TemplatePageHeaderProps = {
   template: TemplateGetStaticProps['template']
@@ -7,32 +10,43 @@ type TemplatePageHeaderProps = {
 
 export const TemplatePageHeader = (props: TemplatePageHeaderProps) => {
   const { template } = props
+
+  const router = useRouter()
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(
+      process.env.NEXT_PUBLIC_APP_URL + router.asPath
+    )
+  }
+
+  const createDialog = useState(false)
+
+  const openCreateDialog = () => {
+    createDialog[1](true)
+  }
+
   return (
-    <div className="inline-flex justify-between w-full items-center gap-y-4">
-      <h1 className="text-[32px] font-normal">
-        {template?.name || 'Unnamed Template'}
-      </h1>
+    <>
+      <CreateAgentDialog templateId={template.id} state={createDialog} />
+      <div className="inline-flex justify-between w-full items-center gap-y-4">
+        <h1 className="text-[32px] font-normal">
+          {template?.name || 'Unnamed Template'}
+        </h1>
 
-      <div className="inline-flex">
-        <Button
-          variant="portal-neutral"
-          className="mr-4"
-          onClick={() => {
-            console.log('Share')
-          }}
-        >
-          Share
-        </Button>
+        <div className="inline-flex">
+          <Button
+            variant="portal-neutral"
+            className="mr-4"
+            onClick={copyToClipboard}
+          >
+            Share
+          </Button>
 
-        <Button
-          variant="portal-primary"
-          onClick={() => {
-            console.log('Build in Magick')
-          }}
-        >
-          Build in Magick
-        </Button>
+          <Button variant="portal-primary" onClick={openCreateDialog}>
+            Build in Magick
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
