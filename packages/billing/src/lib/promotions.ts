5a068978589755f60f2bcf86b74495171ddfa7d4
Jakob <haruhunab1320@gmail.com>
2024-05-10 10:08:21 -0700

    install keywords to promotions.ts

--- packages/billing/src/lib/promotions.ts
+++ packages/billing/src/lib/promotions.ts
@@ -2,7 +2,7 @@ import { prismaPortal, PromotionType } from '@magickml/portal-db'
 import { Decimal } from '@prisma/client/runtime/library'
 
 export const makeTrialPromotion = async (userId: string) => {
-  await prismaPortal.promotion.create({
+  const promo = await prismaPortal.promotion.create({
     data: {
       name: 'Trial',
       description: 'Get started for free with $2,00 in credits',
@@ -16,6 +16,8 @@ export const makeTrialPromotion = async (userId: string) => {
       userId,
     },
   })
+
+  return promo
 }
 
 export const makeApprenticePromotion = async (userId: string) => {
@@ -38,35 +40,63 @@ export const makeApprenticePromotion = async (userId: string) => {
 }
 
 export const makeWizardPromotion = async (userId: string) => {
-  await prismaPortal.promotion.create({
-    data: {
-      name: 'Wizard',
-      description: 'Wizard subscription credits.',
-      type: PromotionType.ADMIN,
-      amount: new Decimal(10),
-      validFrom: new Date(),
-      validUntil: new Date(new Date().setDate(new Date().getDate() + 30)),
-      isUsed: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      userId,
-    },
-  })
+  try {
+    const promo = await prismaPortal.promotion.create({
+      data: {
+        name: 'Wizard',
+        description: 'Wizard subscription credits.',
+        type: PromotionType.ADMIN,
+        amount: new Decimal(10),
+        validFrom: new Date(),
+        validUntil: new Date(new Date().setDate(new Date().getDate() + 30)),
+        isUsed: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId,
+      },
+    })
+
+    const success = await fetch(
+      `${process.env.KEYWORDS_API_URL}/api/user/update/MP_${userId}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.KEYWORDS_API_KEY}`,
+        },
+        body: JSON.stringify({
+          budget_duration: 'monthly',
+          period_budget: new Decimal(10),
+          period_start: new Date().toISOString(),
+        }),
+      }
+    ).then(res => res.json())
+
+    if (success?.id) {
+      await prismaPortal.promotion.update({
+        where: { id: promo.id },
+        data: { isUsed: true },
+      })
+    }
+  } catch (error) {
+    console.error('Error updating user:', error)
+    throw error
+  }
 }
 
-export const makeBalancePromotion = async (userId: string, amount: number) => {
-  await prismaPortal.promotion.create({
-    data: {
-      name: 'Balance',
-      description: 'Balance credits.',
-      type: PromotionType.ADMIN,
-      amount: new Decimal(amount),
-      validFrom: new Date(),
-      validUntil: new Date(new Date().setDate(new Date().getDate() + 30)),
-      isUsed: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      userId,
-    },
-  })
-}
+// export const makeBalancePromotion = async (userId: string, amount: number) => {
+//   await prismaPortal.promotion.create({
+//     data: {
+//       name: 'Balance',
+//       description: 'Balance credits.',
+//       type: PromotionType.ADMIN,
+//       amount: new Decimal(amount),
+//       validFrom: new Date(),
+//       validUntil: new Date(new Date().setDate(new Date().getDate() + 30)),
+//       isUsed: false,
+//       createdAt: new Date(),
+//       updatedAt: new Date(),
+//       userId,
+//     },
+//   })
+// }
