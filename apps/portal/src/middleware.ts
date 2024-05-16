5c6036ac4fc8f9fcc110376a863e2adb05457938
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-05-16 19:09:25 -0400

    Refactor middleware to use Clerk's new server-side API

--- apps/portal/src/middleware.ts
+++ apps/portal/src/middleware.ts
@@ -1,41 +1,54 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  debug: process.env.CLERK_MIDDLEWARE_LOGGING === 'true',
-  publicRoutes: [
-    '/auth/sign-in',
-    '/auth/sign-up',
-    '/auth/verify',
-    '/terms',
-    '/privacy',
-    '/eula',
-    '/404',
-    '/api',
-    '/api/auth/sync',
-    '/api/billing/sync',
-    '/subscribe',
-    'templates/(.*)',
-    '/api/trpc/templates.find',
-    '/templates/(.*)',
-    `/templates`,
-  ],
-  ignoredRoutes: [
-    '/api/magick',
-    '/api/magick/budget/set_budget',
-    '/api/magick/budget/get_budget',
-    '/api/magick/user',
-    '/api/magick/user(.*)',
-  ],
-})
+const isPublicRoute = createRouteMatcher([
+  '/auth/sign-in',
+  '/auth/sign-up',
+  '/auth/verify',
+  '/terms',
+  '/privacy',
+  '/eula',
+  '/404',
+  '/api',
+  '/api/auth/sync',
+  '/api/billing/sync',
+  '/subscribe',
+  'templates/(.*)',
+  '/api/trpc/templates.find',
+  '/templates/(.*)',
+  '/templates',
+])
+
+const isIgnoredRoute = createRouteMatcher([
+  '/api/magick',
+  '/api/magick/budget/set_budget',
+  '/api/magick/budget/get_budget',
+  '/api/magick/user',
+  '/api/magick/user(.*)',
+])
+
+export default clerkMiddleware(
+  (auth, req) => {
+    if (isIgnoredRoute(req)) {
+      // Skip auth check for ignored routes
+      return
+    }
+    if (!isPublicRoute(req)) {
+      // Protect routes that are not public
+      auth().protect()
+    }
+  },
+  { debug: process.env.CLERK_MIDDLEWARE_LOGGING === 'true' }
+)
 
 export const config = {
-  matcher: [
-    '/((?!.+\\.[\\w]+$|_next).*)',
-    '/',
-    '/(api|trpc)(.*)',
-    '/agents(.*)',
-    '/projects(.*)',
-    '/account(.*)',
-    '/billing(.*)',
-  ],
+  matcher: ['/(.*)'], // temp match all
+  // '/((?!.+\\.[\\w]+$|_next).*)',
+  // '/',
+  // '/(api|trpc)(.*)',
+  // '/agents(.*)',
+  // '/projects(.*)',
+  // '/account(.*)',
+  // '/billing(.*)',
+  // '/templates(.*)', // Ensure /templates and its subroutes are matched
+  // ],
 }
