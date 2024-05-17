b5f95ed6ba6e4a3885ba0596197e279f2c87d12d
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-05-17 14:33:35 -0400

    i tihnk i should commit theese?

--- packages/db/src/lib/prisma/client-portal/edge.js
+++ packages/db/src/lib/prisma/client-portal/edge.js
@@ -403,7 +403,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/jakobgrant/Workspaces/Oneirocom/Magick/portal/cloud/packages/db/src/lib/prisma/client-portal",
+      "value": "/home/coffee/Desktop/MAGICK/portal/cloud/packages/db/src/lib/prisma/client-portal",
       "fromEnvVar": null
     },
     "config": {
@@ -413,7 +413,7 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "darwin-arm64",
+        "value": "debian-openssl-3.0.x",
         "native": true
       }
     ],
