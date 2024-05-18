65a6055435f7a98669bfde96e2d3d1580fd295f9
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-05-18 03:45:56 -0400

    center clerk sigin pages

--- apps/portal/src/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/portal/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn path="/sign-in" />
+  return (
+    <div className="flex w-full h-full items-center justify-center">
+      <SignIn path="/sign-in" />
+    </div>
+  )
 }
