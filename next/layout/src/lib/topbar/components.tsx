30eccc70db850f9e1c7747e6917eea3e06f8187b
Jakob <haruhunab1320@gmail.com>
2024-06-12 20:47:46 -0700

    feat: Add AgentMenu component to topbar
    This commit adds the AgentMenu component to the topbar in the layout library. The AgentMenu component provides a dropdown menu for managing agents. It fetches agent data from the server and allows users to toggle the menu and view their agents. This feature enhances the functionality of the topbar and improves the user experience.

--- next/layout/src/lib/topbar/components.tsx
+++ next/layout/src/lib/topbar/components.tsx
@@ -1,6 +1,17 @@
 'use client'
+
 import { Button } from 'client/core'
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
+import { SmartToyOutlined } from '@mui/icons-material'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@magickml/client-ui'
+import { useEffect, useState } from 'react'
+import { api } from '@magickml/portal-server-provider'
+import { useUser } from '@clerk/nextjs'
+import { useInView } from 'react-intersection-observer'
 
 export const SubscribeButton = ({
   hasSubscription,
@@ -48,17 +59,66 @@ export const HomeButton = () => {
   )
 }
 
-export const ToggleAgentSideBar = ({
-  toggleAgentSideBar,
-}: {
-  toggleAgentSideBar: () => void
-}) => {
+export const AgentMenu = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false)
+  const { isSignedIn } = useUser()
+  const { inView } = useInView()
+
+  const { data, fetchNextPage } = api.agents.getInfinite.useInfiniteQuery(
+    {},
+    {
+      getNextPageParam: lastPage => lastPage.nextCursor,
+      refetchInterval: 1000 * 60 * 1, // 1 minute
+      enabled: isSignedIn,
+    }
+  )
+
+  useEffect(() => {
+    if (inView && data) {
+      fetchNextPage()
+    }
+  }, [inView, data])
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu)
+  }
+
+  console.log('hello', data)
+
   return (
-    <Button
-      className="bg-[--ds-neutral] text-[--ds-white] px-1 py-1 w-[136px] h-[30px] rounded font-semibold hover:bg-[#919eaa] transition duration-300"
-      onClick={toggleAgentSideBar}
-    >
-      Toggle Agent Side Bar
-    </Button>
+    <DropdownMenu open={!!openMenu} onOpenChange={toggleMenu}>
+      <DropdownMenuTrigger
+        onClick={() => {
+          toggleMenu()
+        }}
+      >
+        <SmartToyOutlined
+          className={`h-4 w-4 m-1 ${openMenu ? 'text-[#06c9f0]' : ''}`}
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className={`w-[200px] border-[1.5px] border-[#06c9f0] bg-[#060607] flex flex-col items-center`}
+        style={{ marginLeft: '110px' }}
+      >
+        <h3 className="mt-2 mb-2 pb-2 text-white font-semibold border-b-[1px] border-[#181c1c] w-full text-center">
+          My Agents
+        </h3>
+        <div className="w-full overflow-y-auto max-h-64">
+          {data?.pages
+            .flatMap(page => (page.items ? page.items : []))
+            .map((agent, i: number) => (
+              <Button
+                key={agent.id}
+                className={`w-full text-left px-3 py-2 hover:bg-[#363d42] transition duration-300 border-b-[1px] border-[#181c1c]`}
+                onClick={() => {
+                  window.open(`/projects/${agent.projectId}`, '_blank')
+                }}
+              >
+                {agent.name}
+              </Button>
+            ))}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
