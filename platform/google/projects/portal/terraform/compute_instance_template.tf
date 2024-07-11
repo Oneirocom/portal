6c66a464ef8c3b1af1cfeea855c17a1ad69dbe0e
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-07-11 03:28:48 -0400

    refactor: Update Google Compute Instance Template

--- platform/google/projects/portal/terraform/compute_instance_template.tf
+++ platform/google/projects/portal/terraform/compute_instance_template.tf
@@ -1,26 +1,43 @@
 resource "google_compute_instance_template" "portal_instance_template" {
-  disk {
-    boot         = true
-    source_image = "debian-cloud/debian-11"
-  }
+  name        = "portal-instance-template"
+  description = "This template uses a container image from Google's sample GCR registry"
 
-  machine_type            = "e2-medium"
-  metadata_startup_script = "echo \"helloworld!\""
-  name                    = "portal-instance-template"
+  machine_type = "e2-medium"
+
+  disk {
+    source_image = "cos-cloud/cos-stable"
+    auto_delete  = true
+    boot         = true
+  }
 
   network_interface {
-    network            = google_compute_network.portal_network.self_link
-    subnetwork         = google_compute_subnetwork.portal_subnetwork.self_link
-    subnetwork_project = google_project.portal_project.project_id
+    network    = google_compute_network.portal_network.self_link
+    subnetwork = google_compute_subnetwork.portal_subnetwork.self_link
   }
-
-  project = google_project.portal_project.project_id
-  region  = "us-east4"
 
   service_account {
     email  = google_service_account.compute_service_account.email
     scopes = ["cloud-platform"]
   }
+
+  metadata = {
+    gce-container-declaration = yamlencode({
+      spec = {
+        containers = [{
+          image = "gcr.io/google-samples/hello-app:1.0"
+          name  = "hello-app"
+        }]
+        restartPolicy = "Always"
+      }
+    })
+  }
+
+  labels = {
+    container-vm = "cos-stable"
+  }
+
+  project = google_project.portal_project.project_id
+  region  = "us-east4"
 
   depends_on = [google_project_iam_member.compute_service_account_roles]
 }
