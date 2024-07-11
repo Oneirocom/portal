6a7aadfd6b6294bbbb1faf8af89eed1ee0b4442b
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-07-11 04:01:20 -0400

    refactor: Add Google Artifact Registry, Cloud Build, and Secret Manager services to project_services.tf

--- platform/google/projects/portal/terraform/project_services.tf
+++ platform/google/projects/portal/terraform/project_services.tf
@@ -3,14 +3,16 @@ resource "google_project_service" "services" {
     "compute.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "iam.googleapis.com",
-    "servicenetworking.googleapis.com"
+    "servicenetworking.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "secretmanager.googleapis.com"
   ])
 
   project = google_project.portal_project.project_id
-
   service = each.key
 
-  disable_on_destroy = false
+  disable_on_destroy         = false
   disable_dependent_services = true
 }
 
@@ -19,4 +21,11 @@ resource "time_sleep" "wait_30_seconds" {
   depends_on = [google_project_service.services]
 
   create_duration = "30s"
+}
+
+# Add an additional delay for Cloud Build service account creation
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [time_sleep.wait_30_seconds]
+
+  create_duration = "60s"
 }
