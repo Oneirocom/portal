de0f1cd73acd0e62b8bbc4ad55f41d31e8eccd2b
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2024-07-12 13:43:02 -0400

    cleanup

--- platform/google/projects/portal/terraform/compute_global_forwarding_rule.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_global_forwarding_rule" "portal_forwarding_rule" {
-  ip_address            = google_compute_global_address.portal_global_address.self_link
-  ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL_MANAGED"
-  name                  = "portal-forwarding-rule"
-  port_range            = "80"
-  project               = google_project.portal_project.project_id
-  target                = google_compute_target_http_proxy.portal_http_proxy.self_link
-}
--- platform/google/projects/portal/terraform/compute_network.tf
+++
@@ -1,8 +0,0 @@
-resource "google_compute_network" "portal_network" {
-  name                    = "portal-network"
-  auto_create_subnetworks = false
-  project                 = google_project.portal_project.project_id
-
-
-  depends_on = [time_sleep.wait_30_seconds]
-}
